3c3b5f808d221cc582b9e9c5d921f415fc01aed2
ankitpandey1410 <125684719+ankitpandey1410@users.noreply.github.com>
2023-07-30 01:11:50 +0530

    Add files via upload

---
+++ flags.js
@@ -0,0 +1,115 @@
+function brazilFunc()
+{
+  var c = "This is the flag of Brazil";
+  document.getElementById("brazil").innerHTML = c;
+}
+
+function outBrazil()
+{
+  var c = "";
+  document.getElementById("brazil").innerHTML = c;
+}
+
+
+function indiaFunc()
+{
+  var c = "This is the flag of India";
+  document.getElementById("india").innerHTML = c;
+}
+
+function outIndia()
+{
+  var c = "";
+  document.getElementById("india").innerHTML = c;
+}
+
+
+function englandFunc()
+{
+  var c = "This is the flag of England";
+  document.getElementById("england").innerHTML = c;
+}
+
+function outEngland()
+{
+  var c = "";
+  document.getElementById("england").innerHTML = c;
+}
+
+
+function russiaFunc()
+{
+  var c = "This is the flag of Russia";
+  document.getElementById("russia").innerHTML = c;
+}
+
+function outRussia()
+{
+  var c = "";
+  document.getElementById("russia").innerHTML = c;
+}
+
+
+function chinaFunc()
+{
+  var c = "This is the flag of China";
+  document.getElementById("china").innerHTML = c;
+}
+
+function outChina()
+{
+  var c = "";
+  document.getElementById("china").innerHTML = c;
+}
+
+
+function germanyFunc()
+{
+  var c = "This is the flag of Germany";
+  document.getElementById("germany").innerHTML = c;
+}
+
+function outGermany()
+{
+  var c = "";
+  document.getElementById("germany").innerHTML = c;
+}
+
+
+function franceFunc()
+{
+  var c = "This is the flag of France";
+  document.getElementById("france").innerHTML = c;
+}
+
+function outFrance()
+{
+  var c = "";
+  document.getElementById("france").innerHTML = c;
+}
+
+
+function belgiumFunc()
+{
+  var c = "This is the flag of Belgium";
+  document.getElementById("belgium").innerHTML = c;
+}
+
+function outBelgium()
+{
+  var c = "";
+  document.getElementById("belgium").innerHTML = c;
+}
+
+
+function argentinaFunc()
+{
+  var c = "This is the flag of Argentina";
+  document.getElementById("argentina").innerHTML = c;
+}
+
+function outArgentina()
+{
+  var c = "";
+  document.getElementById("argentina").innerHTML = c;
+}
